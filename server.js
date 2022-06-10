fd6c8d83734921efb2f9f41b41f43c156de1b54f
Vova <romaniuk.volodymyr.clg@chnu.edu.ua>
2022-06-10 11:08:14 +0300

    gfdgdfjhgkh

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname,"./client/build")));
+
 //app.use(require('./controlers/ProductsController'));
 
 // Global error handling
